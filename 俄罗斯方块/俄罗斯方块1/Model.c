cfcc6e9aa5807cba823fd060027708290fea7467
yulongli6 <18709202005@163.com>
2018-10-09 18:09:47 +0800

    10.9

---
+++ 俄罗斯方块/俄罗斯方块1/Model.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+
+#include "Model.h"
+#include "Windows.h"
+#include "view.h"
+
+void Init_dia()
+{
+	for (int i = 0; i < 3;i++)
+	{
+		                            //0 1 0 0
+		dia[0][0].space[1][i] = 1;  //1 1 1 0
+		dia[0][0].space[0][1] = 1;  //0 0 0 0
+		                            //0 0 0 0
+	}
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		                            //1 1 1 0
+		dia[1][0].space[0][i] = 1;  //1 0 0 0
+		dia[1][0].space[1][0] = 1;  //0 0 0 0
+		                            //0 0 0 0
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		                            //1 1 1 0
+		dia[2][0].space[0][i] = 1;  //0 0 1 0
+		dia[2][0].space[1][2] = 1;  //0 0 0 0
+		                            //0 0 0 0
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+			                                //1 1 0 0
+		dia[3][0].space[0][i] = 1;          //0 1 1 0
+		dia[3][0].space[1][i + 1] = 1;      //0 0 0 0
+			                                //0 0 0 0
+		
+			                                //0 1 1 0
+		dia[4][0].space[0][i + 1] = 1;      //1 1 0 0
+		dia[4][0].space[1][i] = 1;          //0 0 0 0
+			                                //0 0 0 0
+
+		                                //1 1 0 0
+		dia[5][0].space[0][i] = 1;      //1 1 0 0
+		dia[5][0].space[1][i] = 1;      //0 0 0 0
+		                                //0 0 0 0
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		                                //1 1 1 1
+		dia[6][0].space[0][i] = 1;      //0 0 0 0
+		                                //0 0 0 0
+		                                //0 0 0 0
+	}
+
+	int tmp[4][4];
+	for (int i = 0; i < 7; i++)      //方块种类
+	{
+		for (int j = 0; j < 3; j++)  //旋转次数
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				for (int z = 0; z < 4; z++)
+				{
+					tmp[k][z] = dia[i][j].space[k][z];
+				}
+			}
+
+			for (int k = 0; k < 4; k++)
+			{
+				for (int z = 0; z < 4; z++)
+				{
+					dia[i][j + 1].space[k][z] = tmp[4 - z - 1][k];
+				}
+			}
+		}
+	}
+}
+
+void GameInit(Game *pGame)
+{
+	pGame->score = 0;
+	pGame->speed = 300;
+	display_face(&pGame->face, pGame->maxscore, pGame->score);
+}
+
+
+
+
+
+int color(int c)
+{
+	switch (c)
+	{
+	case 0: c = 9; break;
+	case 1:
+	case 2: c = 12; break;
+	case 3:
+	case 4: c = 14;break;
+	case 5: c = 10;break;
+	case 6: c = 13;break;
+	default: c = 7; break;
+	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	return 0;
+}
---
+++ 俄罗斯方块/俄罗斯方块1/view.c
@@ -0,0 +1,164 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+
+#include "Model.h"
+#include "Windows.h"
+#include "stdio.h"
+
+ void SetCurPos(int X, int Y)
+{
+	HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	// 标准输入/标准输出/标准错误输出
+	COORD coord = { X, Y };
+	//设置终端光标位置
+	SetConsoleCursorPosition(hStdOutput, coord);
+}
+
+
+void hide_cursor()
+{
+	HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	GetConsoleCursorInfo(hStdOutput, &info);
+	info.bVisible = 0;
+	SetConsoleCursorInfo(hStdOutput, &info);
+}
+
+void dispay_menu()
+{
+	SetCurPos(27, 14);
+	printf("俄罗斯方块");
+	SetCurPos(27, 16);
+	system("pause");
+	system("cls");
+}
+
+void display_face(Face* face, int maxscore, int score)
+{
+	int i, j;
+	for (i = 0; i < FACE_Y; i++)
+	{
+		for (j = 0; j < FACE_X + 10; j++)
+		{
+			if (j == 0 || j == FACE_X - 1 || j == FACE_X + 9)
+			{
+				face->data[i][j] = WALL;
+				SetCurPos(2 * j, i);
+				printf("█");
+			}
+			else if (i == FACE_Y - 1)
+			{
+				face->data[i][j] = BOX;
+				SetCurPos(2 * j, i);
+				printf("█");
+			}
+			else
+			{
+				face->data[i][j] = KONG;
+			}
+		}
+	}
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 18);
+	printf("左移:←");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 16);
+	printf("右移:→");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 14);
+	printf("旋转:space");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 12);
+	printf("暂停: F1");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 10);
+	printf("退出: ESC");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 8);
+	printf("重新开始: F2");
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 6);
+	printf("最高记录:%d", maxscore);
+
+	SetCurPos(2 * FACE_X + 2, FACE_Y - 4);
+	printf("分数:%d", score);
+}
+
+
+void display_kong(int nn, int space_c, int x, int y)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			SetCurPos(2 * (x + j), i + y);
+			if (dia[nn][space_c].space[i][j]==1)
+			{
+				printf("  ");
+			}
+		}
+	}
+}
+
+
+display_kong1(int nn, int space_c, int x, int y)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			SetCurPos(2 * (x + j), i + y);	
+				printf("  ");	
+		}
+	}
+}
+
+
+
+void display_dia(int nn, int space_c, int x, int y)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			SetCurPos(2 * (x + j), i + y);
+			if (dia[nn][space_c].space[i][j] == 1)
+			{
+				printf("█");
+			}
+		}
+	}
+}
+
+
+
+void read_maxscore(int* maxsorse)
+{
+	FILE* pf;
+	pf = fopen("俄罗斯方块记录.txt", "rb");
+	if (NULL == pf)
+	{
+		perror("open file for read");
+		exit(EXIT_FAILURE);
+	}
+	fread(maxsorse, sizeof(int), 1, pf);
+
+	fclose(pf);
+	pf = NULL;
+}
+
+
+void write_maxscore(int* maxsorse)
+{
+	FILE* pf;
+	pf = fopen("俄罗斯方块记录.txt", "wb");
+	if (NULL == pf)
+	{
+		perror("open file for write");
+		exit(EXIT_FAILURE);
+	}
+
+	fwrite(maxsorse, sizeof(int), 1, pf);
+
+	fclose(pf);
+	pf = NULL;
+}
